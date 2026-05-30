70375902914f7b5cb6707623f98a1b82c9a9a945
Dmitriy Ryajov <dryajov@gmail.com>
2026-05-30 16:54:44 -0600

    add tests for new scheduling mechanism

--- tests/promethei/blockexchange/testpendingblocks.nim
+++ tests/promethei/blockexchange/testpendingblocks.nim
@@ -182,7 +182,6 @@ suite "PendingBlocks ownership model":
     pb.markRequested(address, peerCtx)
 
     check pb.isRequested(address)
-    # markRequested decrements retries internally now
     check pb.retries(address) == retriesBefore - 1
     check pb.getRequestPeer(address) == peerCtx.id.some
     check address in peerCtx.blocksRequested
@@ -233,6 +232,7 @@ suite "PendingBlocks ownership model":
     check address in peerCtx.blocksRequested
 
     await pb.resolve(address, blk)
+    check address notin peerCtx.blocksRequested
     let delivery = await handle
     check delivery.blk == blk
     check delivery.address == address
