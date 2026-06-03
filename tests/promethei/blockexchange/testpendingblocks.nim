2fc48d5b132963a9f4d2f78e2fe42e7aa956f2dc
Dmitriy Ryajov <dryajov@gmail.com>
2026-06-03 14:49:23 -0600

    more tests for scheduling pipeline

--- tests/promethei/blockexchange/testpendingblocks.nim
+++ tests/promethei/blockexchange/testpendingblocks.nim
@@ -232,8 +232,8 @@ suite "PendingBlocks ownership model":
     check address in peerCtx.blocksRequested
 
     await pb.resolve(address, blk)
-    check address notin peerCtx.blocksRequested
     let delivery = await handle
+
     check delivery.blk == blk
     check delivery.address == address
     check eventually address notin pb
