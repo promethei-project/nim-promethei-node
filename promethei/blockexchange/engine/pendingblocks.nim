309d85441aacca9c4192a20ee63247737964d80c
Dmitriy Ryajov <dryajov@gmail.com>
2026-06-12 09:35:32 -0600

    ajust batching params & tighten flow
    - adjut batch to 128, since we have a 100mb limit of libp2p messages
    - add explicit block state transitions - make sure that flow is consistent through the pipeline

--- promethei/blockexchange/engine/pendingblocks.nim
+++ promethei/blockexchange/engine/pendingblocks.nim
@@ -532,13 +532,13 @@ proc validateBlock(
   return true
 
 proc validateBlock(
-    self: PendingBlocksManager, address: BlockAddress
+    self: PendingBlocksManager, address: BlockAddress, state: BlockReqState
 ): Future[bool] {.async: (raises: [CancelledError]).} =
   without req =? self.blocks .? [address]:
     trace "Address is not pending", address
     return false
 
-  if req.state in {Dispatching, InFlight}:
+  if req.state != state or not req.requestedPeer.isNil:
     trace "Address already in pipeline, skipping", address, state = req.state
     return false
 
