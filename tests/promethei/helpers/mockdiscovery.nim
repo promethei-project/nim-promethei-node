durability-labs/archivist-node

Advertise without retry window; rework discovery backoff
- Remove the 10-minute advertise retry window: the sparse retry is now
  steady-state, gated only by table health, so small networks keep
  replicating provider records to newly-arriving peers
- Rework the discovery find-backoff to a (due, mult) tuple table with
  multiplier-derived intervals; prune entries once a cid leaves the want
  list, including tree cids of leaf wants
- Make nodesDiscovered a method so tests can override it; export
  DefaultMinAdvertisePeers
- Add steady-state advertiser retry test and backoff-recovery test

tests/archivist/helpers/mockdiscovery.nim
@@ -11,9 +11,12 @@ import pkg/chronos
 import pkg/libp2p
 import pkg/questionable
 import pkg/archivist/discovery
+import pkg/archivist/blockexchange/engine
 import pkg/contractabi/address as ca
 
 type MockDiscovery* = ref object of Discovery
+  nodesDiscoveredHandler*: proc(d: MockDiscovery): int {.gcsafe, raises: [].}
+
   findBlockProvidersHandler*: proc(
     d: MockDiscovery, cid: Cid
   ): Future[seq[SignedPeerRecord]] {.async: (raises: [CancelledError]).}
@@ -31,6 +34,13 @@ type MockDiscovery* = ref object of Discovery
 
 proc new*(T: type MockDiscovery): MockDiscovery =
   MockDiscovery()
+
+method nodesDiscovered*(d: MockDiscovery): int =
+  # Report a healthy table unless a test overrides it
+  if isNil(d.nodesDiscoveredHandler):
+    return DefaultMinAdvertisePeers
+
+  d.nodesDiscoveredHandler(d)
 
 proc findPeer*(
     d: Discovery, peerId: PeerId

promethei-project/nim-promethei-node

tests/promethei/helpers/mockdiscovery.nim
@@ -11,9 +11,12 @@ import pkg/chronos
 import pkg/libp2p
 import pkg/questionable
 import pkg/promethei/discovery
+import pkg/promethei/blockexchange/engine
 import pkg/contractabi/address as ca
 
 type MockDiscovery* = ref object of Discovery
+  nodesDiscoveredHandler*: proc(d: MockDiscovery): int {.gcsafe, raises: [].}
+
   findBlockProvidersHandler*: proc(
     d: MockDiscovery, cid: Cid
   ): Future[seq[SignedPeerRecord]] {.async: (raises: [CancelledError]).}
@@ -31,6 +34,13 @@ type MockDiscovery* = ref object of Discovery
 
 proc new*(T: type MockDiscovery): MockDiscovery =
   MockDiscovery()
+
+method nodesDiscovered*(d: MockDiscovery): int =
+  # Report a healthy table unless a test overrides it
+  if isNil(d.nodesDiscoveredHandler):
+    return DefaultMinAdvertisePeers
+
+  d.nodesDiscoveredHandler(d)
 
 proc findPeer*(
     d: Discovery, peerId: PeerId